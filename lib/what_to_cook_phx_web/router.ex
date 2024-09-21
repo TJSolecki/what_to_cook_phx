55ba892a376657c5daa8bcac8e353bc04f1d7a94
Thomas Solecki <thomasjsolecki@gmail.com>
2024-09-20 23:53:18 -0700

    [view] Setup redirecting from home to login when not auth

--- lib/what_to_cook_phx_web/router.ex
+++ lib/what_to_cook_phx_web/router.ex
@@ -18,7 +18,7 @@ defmodule WhatToCookPhxWeb.Router do
   end
 
   scope "/", WhatToCookPhxWeb do
-    pipe_through :browser
+    pipe_through [:browser, :require_authenticated_user]
 
     get "/", PageController, :home
   end
